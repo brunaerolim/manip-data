a93b2093d384b421ea0ebd4963a5e6f3fc623dac
Bruna  Rolim <brunaerrolim@gmail.com>
2023-09-12 14:44:42 -0300

    aprimoramento

--- numeros.py
+++ numeros.py
@@ -1,9 +1,8 @@
 numeros = [255, 256, 257, 258, 259, 260, 261, 262, 263]
-numeros_pares = []
 
 for numero in numeros:
-    if numero %2 == 0:
-        numeros_pares.append(numero)
+    if numero %2 != 0:
+        numeros.remove(numero)
 
 
-print(numeros_pares) 
+print(numeros) 
